e8684df509c76a372a597aa8d306683a0efadcd7
parichoy-mondal <mparichoy@gmail.com>
2025-08-10 14:53:58 +0530

    Add files via upload

---
+++ pari1a.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    arr = [10, 20, 30, 40]
+    ele = 50
+    print("Array before insertion")
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+
+    arr.append(ele)
+
+    print("\nArray after insertion")
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
