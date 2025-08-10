198a49d9c98359d6d9d020bdae00c04b22f81d64
parichoy-mondal <mparichoy@gmail.com>
2025-08-10 14:54:26 +0530

    Add files via upload

---
+++ pari1b.py
@@ -0,0 +1,8 @@
+def largest(arr, n):
+    ans = max(arr)
+    return ans;
+
+if __name__ == '__main__':
+    arr = [10, 324, 45, 90, 70]
+    n = len(arr)
+    print ("Largest in given array ", largest(arr, n))
